21ab2c984d3e6f68bb2a2f0c322cab04a809508d
Artem <artemlyksa@artems-macbook-pro.local>
2019-02-20 17:21:14 +0200

    Implemented queue with array and stacks. Found out that second method has better complexity.

--- Sources/Queue.swift
+++ Sources/Queue.swift
@@ -27,11 +27,65 @@
 /// THE SOFTWARE.
 
 
-//TODO: Queue Protocol
+protocol Queue {
+    associatedtype Element
+    
+    mutating func enqueue(_ element: Element)
+    mutating func dequeue() -> Element?
+    var isEmpty: Bool { get }
+    var peek: Element? { get }
+}
 
 
 //TODO: Queue Array
 
+struct QueueArray<T>: Queue {
+    private var array: [T] = []
+    
+    var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+    var peek: T? {
+        return array.first
+    }
+    
+    mutating func enqueue(_ element: T) {
+        array.append(element)
+    }
+    
+    @discardableResult
+    mutating func dequeue() -> T? {
+        return isEmpty ? nil : array.removeFirst()
+    }
+}
 
-//TODO: Queue Stack
+
+struct QueueStack<T>: Queue {
+    
+    private var dequeueStack: [T] = []
+    private var enqueueStack: [T] = []
+    
+    var isEmpty: Bool {
+        return dequeueStack.isEmpty && enqueueStack.isEmpty
+    }
+    
+    var peek: T? {
+        return dequeueStack.isEmpty ? enqueueStack.first : dequeueStack.last
+    }
+    
+    mutating func enqueue(_ element: T) {
+        enqueueStack.append(element)
+    }
+    
+    @discardableResult
+    mutating func dequeue() -> T? {
+        if dequeueStack.isEmpty {
+            dequeueStack = enqueueStack.reversed()
+            enqueueStack.removeAll()
+        }
+        
+        return dequeueStack.popLast()
+    }
+}
 
